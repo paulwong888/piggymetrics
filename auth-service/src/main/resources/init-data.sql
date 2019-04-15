8117136e59f1e65f993132e055f64c91807e6500
Paul Wong <paulwong888@163.com>
2019-04-15 17:22:41 +0800

    Create init-data.sql

---
+++ auth-service/src/main/resources/init-data.sql
@@ -0,0 +1,5 @@
+insert into 'oauth_client_details'('client_id','client_secret','scope','authorized_grant_types') values
+    ('account-service','111','server','client_credentials,refresh_token'),
+    ('browser',null,'ui','password,refresh_token'),
+    ('statistics-service','111','server','client_credentials,refresh_token'),
+    ('notification-service','111','server','client_credentials,refresh_token'),
